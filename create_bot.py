4cef4dfd7e1e0158f431f679c8d74d4316e311e7
t0rchik <84737528+t0rch1k@users.noreply.github.com>
2022-10-23 16:28:16 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -8,7 +8,7 @@ from aiogram.utils import executor
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token="5780080776:AAHvUGul-6oZViHxUz01PmC9PYBpTo")
+bot = Bot(token="bot token")
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
